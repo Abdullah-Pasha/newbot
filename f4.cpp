7d57f03d30a3a6cbb2d13e605fa0a724f7cae982
Abdullah-Pasha <163038995+Abdullah-Pasha@users.noreply.github.com>
2024-03-20 23:28:41 +0500

    Add files via upload

---
+++ f4.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<sizeof (int)<<" "<<sizeof (long long int)<<"  "<<sizeof (char)<<endl;
+	cout<<sizeof (int*)<<" "<<sizeof (long long int*)<<"  "<<sizeof (char*)<<endl;
+	
+	return 0;
+}
