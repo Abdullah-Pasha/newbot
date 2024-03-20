8c9107fceecf101d3033f3af608bc7597a9b460c
Abdullah-Pasha <163038995+Abdullah-Pasha@users.noreply.github.com>
2024-03-20 23:29:05 +0500

    Add files via upload

---
+++ f3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[6]={10,20,30,40,50,60};
+	
+	
+		cout<<arr[3]<<endl;
+	
+	char name[50]="Ali Raza";
+	
+	cout<<ptr<<endl;
+	cout<<*ptr<<endl;
+	return 0;
+}
